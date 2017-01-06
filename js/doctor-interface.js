727686688be6aadc52a6f0eae6f2e1d03d91f376
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-06 08:32:05 -0800

    Refactor error parameters to use callback function, print error messages to page

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,11 +4,15 @@ function displayDoctors(firstName, lastName, title) {
   $('#doctor-list').append("<li>" + firstName + " " + lastName + ", " + title);
 }
 
+function displayError(error) {
+  $('#error').text(error)
+}
+
 $(document).ready(function(){
   $('#find-doctors').click(function() {
     var ailment = $('#ailment').val();
     var name = $('#name').val();
     var doctor = new Doctor();
-    doctor.findDoctors(ailment, name, displayDoctors);
+    doctor.findDoctors(ailment, name, displayDoctors, displayError);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,13 +2,13 @@ var apiKey = require('./../.env').apiKey;
 
 function Doctor() {}
 
-Doctor.prototype.findDoctors = function(ailment, name, displayDoctors) {
+Doctor.prototype.findDoctors = function(ailment, name, displayDoctors, displayError) {
   if (ailment && !name) {
     console.log("ailment present");
     $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=" + apiKey)
     .then(function(result) {
       if (result.data.length === 0) {
-        console.log('zero');
+        displayError("Sorry, there are no doctors in this area that treat that condition.");
       } else {
         result.data.forEach(function(doctor) {
           var firstName = doctor.profile.first_name;
@@ -19,15 +19,14 @@ Doctor.prototype.findDoctors = function(ailment, name, displayDoctors) {
       }
     })
     .fail(function(error){
-      console.log("fail");
-      // fix this
+      displayError(error.responseJSON.message);
     });
   } else if (name && !ailment) {
     console.log("name present");
     $.get("https://api.betterdoctor.com/2016-03-01/doctors?name=" + name + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=" + apiKey)
     .then(function(result) {
       if (result.data.length === 0) {
-        console.log('zero');
+        displayError("Sorry, there are no doctors in this area with that name.");
       } else {
         result.data.forEach(function(doctor) {
           var firstName = doctor.profile.first_name;
@@ -38,15 +37,14 @@ Doctor.prototype.findDoctors = function(ailment, name, displayDoctors) {
       }
     })
     .fail(function(error){
-      console.log("fail");
-      // fix this
+      displayError(error.responseJSON.message);
     });
   } else {
     console.log("both");
     $.get("https://api.betterdoctor.com/2016-03-01/doctors?name=" + name + "&query=" + ailment + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=" + apiKey)
     .then(function(result) {
       if (result.data.length === 0) {
-        console.log('zero');
+        displayError("Sorry, there are no doctors in this area that meet those parameters.");
       } else {
         result.data.forEach(function(doctor) {
           var firstName = doctor.profile.first_name;
@@ -57,8 +55,7 @@ Doctor.prototype.findDoctors = function(ailment, name, displayDoctors) {
       }
     })
     .fail(function(error){
-      console.log("fail");
-      // fix this
+      displayError(error.responseJSON.message);
     });
   }
 };

a8f0484ce4b25397d00204483bd9b4aaf113e4df
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-05 20:13:21 -0800

    Print doctors to page

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,9 +1,13 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+function displayDoctors(firstName, lastName, title) {
+  $('#doctor-list').append("<li>" + firstName + " " + lastName + ", " + title);
+}
+
 $(document).ready(function(){
   $('#find-doctors').click(function() {
     var ailment = $('#ailment').val();
     var doctor = new Doctor();
-    doctor.findDoctors(ailment);
+    doctor.findDoctors(ailment, displayDoctors);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,22 +2,21 @@ var apiKey = require('./../.env').apiKey;
 
 function Doctor() {}
 
-Doctor.prototype.findDoctors = function(ailment) {
+Doctor.prototype.findDoctors = function(ailment, displayDoctors) {
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
   .then(function(result) {
     result.data.forEach(function(doctor) {
-      console.log(doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title);
+      var firstName = doctor.profile.first_name;
+      var lastName = doctor.profile.last_name;
+      var title = doctor.profile.title;
+      displayDoctors(firstName, lastName, title);
     });
   })
   .fail(function(error){
     console.log("fail");
+    // fix this
   });
 };
 
 
 exports.doctorModule = Doctor;
-
-
-// result.data.profile.first_name
-// last_name
-// title

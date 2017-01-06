59e9b00add466a2e421df2bc721487ae553af39b
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-05 20:06:51 -0800

    Return doctor list to console

--- js/doctor.js
+++ js/doctor.js
@@ -3,9 +3,11 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {}
 
 Doctor.prototype.findDoctors = function(ailment) {
-  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
   .then(function(result) {
-    console.log(result);
+    result.data.forEach(function(doctor) {
+      console.log(doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title);
+    });
   })
   .fail(function(error){
     console.log("fail");
@@ -14,3 +16,8 @@ Doctor.prototype.findDoctors = function(ailment) {
 
 
 exports.doctorModule = Doctor;
+
+
+// result.data.profile.first_name
+// last_name
+// title

7df648c2f5f155713ae769f559fe1753dcb8ed5d
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-05 19:48:21 -0800

    Working API Call

--- js/doctor.js
+++ js/doctor.js
@@ -3,7 +3,13 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {}
 
 Doctor.prototype.findDoctors = function(ailment) {
-  console.log(ailment);
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
+  .then(function(result) {
+    console.log(result);
+  })
+  .fail(function(error){
+    console.log("fail");
+  });
 };
 
 

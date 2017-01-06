0307338e7f4da5538736f3abf7f10dc4bb9c1c1a
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-05 20:31:24 -0800

    Add next steps in readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-is pipeline correct? gulp serve includes gulp build?
+format output
+
+make more intuitive
+
+Allow users to search by name and medical issue.
+Update the UI if a doctor can't be found (this happens when you search by name and issue quite frequently.)
+Add an additional API call to retrieve the list of specialities from the database before you query for a doctor, then return that list in a dropdown menu.
+Add styling with SASS. Use a Gulp task to compile it.
+
+README INSTRUCTIONS
--- js/doctor.js
+++ js/doctor.js
@@ -3,7 +3,7 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {}
 
 Doctor.prototype.findDoctors = function(ailment, displayDoctors) {
-  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + ailment + "&location=45.5231%2C-122.6765%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=" + apiKey)
   .then(function(result) {
     result.data.forEach(function(doctor) {
       var firstName = doctor.profile.first_name;
